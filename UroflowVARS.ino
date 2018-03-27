094cff538878d0fc1fc521426ee5c9641c3593d1
Rok Rodič <4655264+rokrodic@users.noreply.github.com>
2018-03-27 23:16:15 +0300

    Add files via upload

---
+++ UroflowVARS.ino
@@ -0,0 +1,61 @@
+/*
+ *
+ * ROSA Uroflow controller
+ * Select: NODEMCU 1.0 4M(1M SPIFFS) 80MHz
+ * Written by Rok Rodic alias Hacker007 alias GreenEyedExplorer
+ * Homepage: http://www.rodic.si
+ * Thingiverse: https://goo.gl/jdyHbF
+ * GreenEyedExplorer GitHub: https://goo.gl/XtjB3h
+ * Youtube: https://www.youtube.com/channel/UCIOIhhPirDJH8LB0azJmd8w
+ * 
+ * 
+[CNT] measurements made
+[maxCNT] max measurements in free firmware
+[esp8266] Chip ID
+[IP] IP address
+[rssi] RSSI of Wifi
+[ssid] SSID of Wifi
+[pass] password (only in set.html)
+[MAC] MAC address
+[save] should save config?
+[ctim] current time
+[tz] time zone
+[ERR] error string
+[STime] is summer time?
+[RSTr] last reset reason
+[fRAM] free RAM
+[uptime] up time
+[rtim] run time
+[Author] author of firmware
+[IntroS] Intro string
+[host] host name
+[ChipI] deprecated
+[SysI] deprecated
+[LChart] Google chart VOLUME (only in index.html)
+[LChart2] Google chart FLOW (only in index.html)
+[VOL] volume of voided urine (only in index.html)
+[Vtime] voiding time (only in index.html)
+[Fmax] max flow (only in index.html)
+[Favrg] average flow (only in index.html)
+[wcal] (only in /calibration and /wcal)
+[cfact] (only in cal2.html and /wcal)
+[resp] (only in cal2.html and /wcal)
+
+Available pages:
+index.html, index.htm, / main page
+about.html about page
+description.xml SSDP file
+loading.gif
+set.html settings
+/WMreset reset wifi
+/format format filesystem
+/calibration
+/wcal
+/cal3
+/saveCfg reset&save config json file
+/config view config.json
+/ERrst reset error string
+/rokrodic about author
+/set post SSID and password
+/rstWIFI reset wifi & run wifi setup portal
+*/
